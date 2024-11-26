2697bd4c83d03f72f32e00873a0617453f1a6f94
searchmoon <moon901212@gmail.com>
2024-11-26 23:55:02 +0900

    dashboard 튜토리얼chapter 2

--- app/ui/invoices/status.tsx
+++ app/ui/invoices/status.tsx
@@ -1,12 +1,12 @@
 import { CheckIcon, ClockIcon } from '@heroicons/react/24/outline';
-import clsx from 'clsx';
+import clsx from 'clsx'; 
 
 export default function InvoiceStatus({ status }: { status: string }) {
   return (
     <span
       className={clsx(
         'inline-flex items-center rounded-full px-2 py-1 text-xs',
-        {
+        {// 클래스 이름을 쉽게 전환할 수 있는 라이브러리인 clsx 로 css를 스타일을 전환할 수 있다.
           'bg-gray-100 text-gray-500': status === 'pending',
           'bg-green-500 text-white': status === 'paid',
         },
